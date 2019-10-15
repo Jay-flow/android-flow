397d819cdef755f7bb1f09b043db1cf7bb8e72b4
Army-92 <kimpetro153@gmail.com>
2019-10-15 22:23:25 +0900

    ProfileActivity show images work activity

--- app/src/main/java/io/flow/ProfileActivity.kt
+++ app/src/main/java/io/flow/ProfileActivity.kt
@@ -1,14 +1,19 @@
 package io.flow
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import io.data.UserData
 import io.flow.fragments.GalleryFragment
+import io.util.SetImagesNotifierInterface
+import io.util.SetImagesTask
 import io.util.UserSharedPreferences
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_profile.nickname
+import kotlinx.android.synthetic.main.fragment_gallery.*
+import kotlinx.android.synthetic.main.fragment_gallery.view.*
 
 class ProfileActivity : AppCompatActivity() {
     private var user: UserData? = null
@@ -18,7 +23,17 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         user = UserSharedPreferences(this).get()
+        setupImages()
         setupProfile()
+    }
+
+    private fun setupImages() {
+        val galleryFragment = supportFragmentManager.findFragmentById(R.id.upload_gallery) as GalleryFragment
+        SetImagesTask(user?.images, object: SetImagesNotifierInterface {
+            override fun setImagesResult(result: ArrayList<Bitmap>) {
+                galleryFragment.image0.setImageBitmap(result[0])
+            }
+        }).execute()
     }
 
     private fun setupProfile() {
--- app/src/main/java/io/flow/fragments/GalleryFragment.kt
+++ app/src/main/java/io/flow/fragments/GalleryFragment.kt
@@ -1,32 +1,18 @@
 package io.flow.fragments
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.flow.R
-import io.util.SetImagesNotifierInterface
-import io.util.SetImagesTask
-import io.util.UserSharedPreferences
-import kotlinx.android.synthetic.main.fragment_gallery.view.*
 
 class GalleryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val inflate = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val user = activity?.let { UserSharedPreferences(it).get() }
-
-        SetImagesTask(user?.images, object : SetImagesNotifierInterface {
-            override fun setImagesResult(result: ArrayList<Bitmap>) {
-                inflate.image0.setImageBitmap(result[0])
-            }
-        }).execute()
-
-        return inflate
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
     }
 
 
